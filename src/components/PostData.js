faa90a0fefc68de0eb04dd998bfbc6a753c4d38b
Manny <manny@Romanos-Mac-mini.local>
2019-06-04 20:32:54 -0400

    Fake auth

--- src/components/PostData.js
+++ src/components/PostData.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export function PostData(type, userData) {
-    let BaseURL = 'http://dev.codebine.com/frames.php';
+    let BaseURL = 'https://codebine.com/fakeauth';
 
     axios({
         method: 'post',
